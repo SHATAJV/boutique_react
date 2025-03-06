7f3b65787d788ed49fde3394e2cba5c34387e514
shahrzad Tajvidi <shahrzad.tajvidi@fms-ea.com>
2025-03-06 11:57:01 +0100

    add order

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import ProductList from "./components/productList";
+import Order from "./components/order";
 
 const App = () => {
   return (
     <div>
       <h1> Boutique d'Instruments </h1>
       <ProductList />
+      <Order />
     </div>
   );
 };

72c4d1fa64cf721bbb358611c85b763396b5f214
shahrzad Tajvidi <shahrzad.tajvidi@fms-ea.com>
2025-03-06 09:18:31 +0100

    add productList

---
+++ src/components/productList.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import products from "boutique/product.json";
+
+const ProductList = () => {
+  return (
+    <div>
+      <h2>Nos Instruments </h2>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>
+            <h3>{product.name}</h3>
+            <p>{product.description}</p>
+            <p>{product.price} €</p>
+            <button>Ajouter au panier</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ProductList;
